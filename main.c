3e7c1b62c45140b6579fb5334234cc3cc0068236
Steve Wang <swang01@stuy.edu>
2019-12-05 20:27:06 +0000

    worked on main

--- main.c
+++ main.c
@@ -11,7 +11,23 @@ int main(int argc, char *argv[]){
   arg_array = parse_args(line);
   int f = fork();
   int x = getpid();
-  execvp(arg_array[0], arg_array);
-  wait(&x);
+  int y = getppid();
+  int k;
+  int i;
+  if (f == 0){
+    printf("Child");
+    execvp(arg_array[0], arg_array);
+  } else if (f > 0){
+    if ((x = wait(&k)) < 0){
+      printf("error");
+      exit(0);
+    }
+    printf("finished");
+  } else{
+    printf("failed");
+  }
+/*  for (i = 0; i < sizeof(arg_array)/sizeof(arg_array[0]); i++){
+    execvp(arg_array[0], arg_array);
+  } */
 
 }
