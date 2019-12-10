8462fcb7e3748fc64728ad072c53b77d8a1f6220
YifanWang0 <ywang00@stuy.edu>
2019-12-10 15:02:53 -0500

    fixing arg_array

--- main.c
+++ main.c
@@ -22,39 +22,48 @@ int main(int argc, char *argv[]){
     char ** arg_array;
     arg_array = parse_args(line,";");
     if (errno){
-      printf("errno %d error %s\n", errno, strerror(errno));
+        printf("errno: %d; error: %s\n", errno, strerror(errno));
     }
+    int i = 0;
+
+    //checking if parse_args correctly separated list of commands
+    while(arg_array[i] != NULL){
+        printf("%s\n", arg_array[i]);
+        i++;
+    }
+
     char ** arg_array2;
-    int a;
-    int b;
-    for (a = 0; a < sizeof(arg_array)/sizeof(char *); a++){
-      arg_array2 = parse_args(arg_array[a], " ");
-      b = 0;
-      while (arg_array2[b] != NULL){
-        int f = fork();
-        int x;
-        int y = getppid();
-        int status;
-        int i;
-        if (f == 0){
-          printf("Child\n");
-          execvp(arg_array[0], arg_array);
-          printf("execvp failed");
-        } else if (f > 0){
-          if ((x = wait(&status)) < 0){
-            printf("error");
-            exit(0);
-          }
-          printf("finished");
-        } else{
-          printf("failed");
+    int a = 0;
+    while(arg_array[a] != NULL){
+        printf("-----\n");
+        arg_array2 = parse_args(arg_array[a], " ");
+        execvp(arg_array2[0], arg_array2);
+        /**
+        int b = 0;
+        while (arg_array2[b] != NULL){
+            int f = fork();
+            int x;
+            int status;
+            if (f == 0){
+                printf("child\n");
+                execvp(arg_array2[b], arg_array2);
+                printf("execvp failed\n");
+            } else if (f > 0){
+                if ((x = wait(&status)) < 0){
+                    printf("error\n");
+                    exit(0);
+                }
+                printf("finished\n");
+            } else{
+                printf("failed\n");
+            }
+            b++;
         }
-        b++;
-      }
+        **/
+        a++;
     }
     return 0;
-  for (i = 0; i < sizeof(arg_array)/sizeof(arg_array[0]); i++){
-    execvp(arg_array[0], arg_array);
-  } */
-
+    for (i = 0; i < sizeof(arg_array)/sizeof(arg_array[0]); i++){
+        execvp(arg_array[0], arg_array);
+    }
 }
