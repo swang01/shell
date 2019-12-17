666c34ddb5576fdaba6481bdccf53fe1678a82e4
YifanWang0 <ywang00@stuy.edu>
2019-12-16 23:48:15 -0500

    function headers

--- shell.c
+++ shell.c
@@ -8,6 +8,14 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 
+/*
+static void sighandler(int signo)
+
+Input: int signo
+Output: void, if the signo is SIGINT it exits the process
+Description: Prints "terminated" in the shell and exits the
+shell
+*/
 static void sighandler(int signo){
     if (signo == SIGINT){
         printf("Terminated\n");
@@ -15,6 +23,15 @@ static void sighandler(int signo){
     }
 }
 
+/*
+char ** parse_args(char * line, char * delimiter)
+
+Input: char * line, char * delimiter
+Output: A pointer to an array of pointers of chars of items
+of a line of command
+Description: Separates a line of command by the specified
+delimiter
+*/
 char ** parse_args(char * line, char * delimiter){
     char ** args = malloc(6 * sizeof(char *));
     int i = 0;
@@ -26,6 +43,15 @@ char ** parse_args(char * line, char * delimiter){
     return args;
 }
 
+/*
+char * trim(char * str)
+
+Input: char * str
+Output: A pointer to an array of chars of items of edited line
+of command
+Description: Edits extra space in the beginning or end of line of
+command to allow the format "command ; command"
+*/
 char * trim(char * str) {
     int i,j;
     char * output = str;
@@ -48,7 +74,15 @@ char * trim(char * str) {
 }
 
 
-void redirect_in(char * line){
+/*
+void redirect_in(char * line)
+
+Input: char * line
+Output: void, performs redirect in
+Description: Takes in a command line that intends for redirect in,
+parses the line and performs the redirection
+*/
+void redirect_in(char * line) {
   char ** parsed = parse_args(line, "<");
   char ** args = parse_args(args[0], " ");
   int fd = open(parsed[1], O_RDONLY, 0644);
@@ -60,6 +94,14 @@ void redirect_in(char * line){
   exit(1);
 }
 
+/*
+void redirect_out(char * line)
+
+Input: char * line
+Output: void, performs redirect out
+Description: Takes in a command line that intends for redirect out,
+parses the line and performs the redirection
+*/
 void redirect_out(char * line){
   char ** parsed = parse_args(line, ">");
   char ** args = parse_args(args[0], " ");
@@ -72,6 +114,14 @@ void redirect_out(char * line){
   exit(1);
 }
 
+/*
+void redirect_out(char * line)
+
+Input: char * command
+Output: An int indicating the process has successfully ran
+Description: Takes in a command line that intends for piping,
+parses the line and performs the piping
+*/
 int pipes(char * command){
     char ** args = parse_args(command, " ");
   char * input  = args[0];
@@ -90,6 +140,14 @@ int pipes(char * command){
     return 0;
 }
 
+/*
+void changeDir(char * command)
+
+Input: char * command
+Output: Void, performs what "cd" does in a regular shell
+Description: Takes in a command line, parses the command and performs
+the corresponding cd command
+*/
 void changeDir(char * command) {
     char * newDir;
     char * source;
@@ -102,6 +160,13 @@ void changeDir(char * command) {
     }
 }
 
+/*
+int exec_command(char * command)
+
+Input: char * command
+Output: An int indicating that the process ran successfully
+Description: Executes a line of single command, without ";"
+*/
 int exec_command(char * command){
     command = trim(command);
     char ** args = parse_args(command, " ");
@@ -131,6 +196,13 @@ int exec_command(char * command){
     }
 }
 
+/*
+int exec_multiple(char * command)
+
+Input: char * command
+Output: An int indicating that the process ran successfully
+Description: Executes a line of multiple commands, separated by ";"
+*/
 int exec_multiple(char * command){
     char ** args = parse_args(command, ";");
     int i = 0;
